Fix std computation
Should use sample variance instead of population variance. Note that the
difference in this case is very small. And the relative weights remain
unaffected.

inline_slm_calibration/calibration_functions.py
@@ -295,8 +295,8 @@ def learn_field(
     """
     # Initialize
     measurements = torch.tensor(measurements, dtype=torch.float32)
-    stds = stds / measurements.std()
-    measurements = measurements / measurements.std()                                # Normalize by std
+    stds = stds / measurements.std()                                                # Normalize by std of all
+    measurements = measurements / measurements.std()                                # Normalize by std of all
 
     weights = compute_weights(measurements, stds, do_weights_plot)
 

inline_slm_calibration/import_utilities.py
@@ -101,7 +101,7 @@ def import_inline_calibration(inline_file, do_import_plot=False) -> tuple[nd, nd
     print('Importing new inline measurements...')
     npz_data = np.load(inline_file)     # Note: dimension 0 is unused
     measurements = npz_data["frames"].mean(axis=(0, 1, 2)) - npz_data['dark_frame'].mean()
-    stds = npz_data["frames"].std(axis=(0, 1, 2))
+    stds = npz_data["frames"].std(axis=(0, 1, 2), ddof=1)   # = sqrt( sample variance )
     gv0 = npz_data['gray_values1'][0]
     gv1 = npz_data['gray_values2'][0]
 